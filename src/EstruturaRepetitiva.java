1b72e5f7dbdd9e0eabf4201905cc3696a95d8007
douglaslima-ubec <douglas.lima@a.ucb.br>
2024-04-14 16:30:23 -0300

    terminei a lista de exercícios

--- src/EstruturaRepetitiva.java
+++ src/EstruturaRepetitiva.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class EstruturaRepetitiva {
 
@@ -62,9 +63,95 @@ public class EstruturaRepetitiva {
 			opcao = sc.nextInt();
 		}
 		System.out.println("MUITO OBRIGADO");
-		System.out.println("Alcool: "+alcool);
-		System.out.println("Gasolina: "+gasolina);
-		System.out.println("Diesel: "+diesel);
+		System.out.println("Alcool: " + alcool);
+		System.out.println("Gasolina: " + gasolina);
+		System.out.println("Diesel: " + diesel);
+	}
+
+	public static void listarImpares() {
+		int X = sc.nextInt();
+		for (int i = 1; i <= X; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+	public static void calcularIntervalo() {
+		int N = sc.nextInt();
+		int numero;
+		int dentro = 0;
+		int fora = 0;
+		for (int i = 0; i < N; i++) {
+			numero = sc.nextInt();
+			if (numero >= 10 && numero <= 20) {
+				dentro++;
+			} else {
+				fora++;
+			}
+		}
+		System.out.println(dentro + " in");
+		System.out.println(fora + " out");
+	}
+
+	public static void casosDeTeste() {
+		int N = sc.nextInt();
+		double[][] casos = new double[N][3];
+		double[] mediaPonderada = new double[N];
+		for (int i = 0; i < N; i++) {
+			casos[i][0] = sc.nextDouble();
+			casos[i][1] = sc.nextDouble();
+			casos[i][2] = sc.nextDouble();
+			mediaPonderada[i] = (casos[i][0] * 2 + casos[i][1] * 3 + casos[i][2] * 5) / 10;
+		}
+		for (int i = 0; i < N; i++) {
+			System.out.printf("%.1f%n", mediaPonderada[i]);
+		}
+	}
+
+	public static void calcularDivisao() {
+		int N = sc.nextInt();
+		int[][] numeros = new int[N][2];
+		double[] divisao = new double[N];
+		for (int i = 0; i < N; i++) {
+			numeros[i][0] = sc.nextInt();
+			numeros[i][1] = sc.nextInt();
+		}
+		for (int i = 0; i < N; i++) {
+			if (numeros[i][1] == 0) {
+				System.out.println("divisao impossivel");
+			} else {
+				divisao[i] = (double) numeros[i][0] / numeros[i][1];
+				System.out.printf("%.1f%n", divisao[i]);
+			}
+		}
+	}
+
+	public static void calcularFatorial() {
+		int N = sc.nextInt();
+		int fatorial = 1;
+		for (int i = 0; i < N; i++) {
+			fatorial *= N - i;
+		}
+		System.out.println(fatorial);
+	}
+
+	public static void calcularDivisores() {
+		int N = sc.nextInt();
+		for (int i = 1; i <= N; i++) {
+			if (N % i == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+	public static void calcularLinhas() {
+		int N = sc.nextInt();
+		for (int i = 1; i <= N; i++) {
+			int quadrado = (int) Math.pow(i, 2);
+			int cubo = (int) Math.pow(i, 3);
+			System.out.printf("%d %d %d%n",i,quadrado,cubo);
+		}
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,16 @@ import java.util.Locale;
 public class Main {
 
 	public static void main(String[] args) {
-		Locale local = new Locale("en", "US");
+		/*
+		 * A partir do Java versão 19
+		 * A classe Locale deve ser instanciada usando os contrutores:
+		 * 1. Método de fábrica estático Locale.of(String language, String country, String variant)
+		 * 2. Método de fábrica estático Locale.forLanguageTag(String BCP47Tag)
+		 * 3. Classe Locale.Builder
+		 */
+		Locale local = Locale.forLanguageTag("en-US");
 		Locale.setDefault(local);
-		EstruturaRepetitiva.abastecerCombustivel();
+		EstruturaRepetitiva.calcularLinhas();
 	}
 
 }
